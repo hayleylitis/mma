169d11e71ac445f849e45be4d38601a6872c4478
marcela moura <marcelacristinarm96@gmail.com>
2019-03-01 11:47:30 -0300

    kudshfiuwhgf

---
+++ Vinicius.java
@@ -0,0 +1,15 @@
+public class Vinicius {
+
+	public static void main(String[] args) {
+		double var1= 0;
+		double var2= 1;
+		double var3= 2;
+		double var4= 3;
+		double var5= 4;
+		double var6= 5;
+		double media= (var1+var2+var3+var4+var5+var6)/6
+
+		System.out.println(media);
+	}
+	
+}
