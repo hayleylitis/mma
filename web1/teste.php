fbbef8692ffa9a606d4cc690bad53f7eb5ca54d1
marcela moura <marcelacristinarm96@gmail.com>
2019-03-01 11:53:51 -0300

    iewuhbfliwg

---
+++ web1/teste.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Meu primeiro php</title>
+</head>
+<body>
+    <?php 
+        for ($i = 0; $i < 10; $i++){
+        	   echo "<li>$i</li>";
+        }
+
+
+     ?>
+</body>
+</html>
