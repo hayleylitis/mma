0d32738e4983c3a2583dfd82574454206d159003
marcela <silvioej@gmail.com>
2019-03-01 11:38:50 -0300

    mamae

---
+++ bruno.java
@@ -0,0 +1,13 @@
+public class media {
+
+public static void main(string[]args){
+	  int a = 4;
+		int b = 6;
+		int resultado;
+
+    resultado = (a + b)/2;
+
+		System.out.println(resultado);
+
+}
+}
